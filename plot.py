37d6262e07818a23ef87701a54c26329c44d22dd
Taylor <taylor.foster23@gmail.com>
2024-03-15 12:22:07 -0700

    cleanup

--- plot.py
+++ plot.py
@@ -2,22 +2,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-def hourToDeg(hour):
-    return hour * (360 / 24)
 
-def hourToRad(hour):
-    return np.deg2rad(hourToDeg(hour))
+def timeToDeg(hour, minute, second):
+    return 360 * ((hour / 24) + (minute / (24 * 60)) + (second / (24 * 60 * 60)))
+
+
+def timeToRad(hour, minute, second):
+    return np.deg2rad(timeToDeg(hour, minute, second))
 
 
 data = pd.read_csv('lab3wedge.csv')
 
 velocities = data['Vr']
-declinations = hourToRad(data['Dec'])
-declinations = (declinations * 2) - hourToRad(14)
+declinations = timeToRad(data['Hours'], data['Minutes'], data['Seconds'])
+declinations = (declinations * 2) - timeToRad(14, 0, 0)
 
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 ax.scatter(declinations, velocities)
-ax.set_theta_offset(hourToRad(16))
+ax.set_theta_offset(timeToRad(16, 0, 0))
 ax.set_xticks(np.linspace(np.pi, 3 * np.pi / 2, 4))
 ax.set_rmax(12000)
 ax.set_rticks([2000, 4000, 6000, 8000, 10000, 12000])
@@ -27,10 +29,10 @@ ax.set_yticklabels(radial_tick_labels)
 
 ax.grid(True)
 
-ax.set_thetamin(hourToDeg(10))
-ax.set_thetamax(hourToDeg(18))
+ax.set_thetamin(timeToDeg(10, 0, 0))
+ax.set_thetamax(timeToDeg(18, 0, 0))
 
-theta_ticks = np.linspace(hourToDeg(10), hourToDeg(18), 5)
+theta_ticks = np.linspace(timeToDeg(10, 0, 0), timeToDeg(18, 0, 0), 5)
 theta_labels = ['12H', '13H', '14H', '15H', '16H']
 ax.set_xticks(np.deg2rad(theta_ticks))
 ax.set_xticklabels(theta_labels)
